1cb9f71fccd3efd00c99b65825016d13ca6f11cf
kh <kylemhouseholder@gmail.com>
2025-10-24 18:25:00 -0500

    type edit

--- pkg/types/weather.go
+++ pkg/types/weather.go
@@ -1,32 +1,30 @@
 package types
 
-// Alias typing pretending to be enums
-type Sky string
+type Sky string // Alias typing (FIAE)
 
 const (
-	SkyCLR Sky = "clear"
-	SkyFEW Sky = "few"
-	SkySCT Sky = "scattered"
-	SkyBKN Sky = "broken"
-	SkyOVC Sky = "overcast"
+	SkyClear     Sky = "clear"
+	SkyFew       Sky = "few"
+	SkyScattered Sky = "scattered"
+	SkyBroken    Sky = "broken"
+	SkyOvercast  Sky = "overcast"
 )
 
-// Subcomponent types =========================================================
 type Timestamp struct {
 	Day  int `json:"day"`
 	Time int `json:"time"`
 }
 
 type WeatherData struct {
-	Station string    `json:"station"`
-	Issued  Timestamp `json:"issued"`
-	METAR   Report    `json:"metar"`
-	TAF     Forecast  `json:"taf"`
+	Station string         `json:"station"`
+	METAR   ComponentMETAR `json:"metar"`
+	TAF     ComponentTAF   `json:"taf"`
 }
 
 type SurfaceWind struct {
 	From  int `json:"from"`
 	Speed int `json:"speed"`
+	Gusts int `json:"gusts"`
 }
 
 type CloudLayer struct {
@@ -41,20 +39,38 @@ type TempData struct {
 	ExactDewpoint float64 `json:"exact_dewpoint"`
 }
 
+type Remarks struct {
+	// TODO: diverse possible contents
+}
+
+type ComponentMETAR struct {
+	RawData string       `json:"raw_data"`
+	Special bool         `json:"special"`
+	Issued  Timestamp    `json:"issued"`
+	Wind    SurfaceWind  `json:"wind"`
+	Clouds  []CloudLayer `json:"clouds"`
+	Vis     int          `json:"vis"`
+	Temps   TempData     `json:"temps"`
+	Baro    float64      `json:"baro"`
+}
+
+type TAFHeader struct {
+	Issued Timestamp
+}
+
 type TAFLine struct {
 	From Timestamp   `json:"from"`
 	Wind SurfaceWind `json:"wind"`
 }
 
-// Component types =========================================================
-type Report struct {
-	RawData string       `json:"raw_data"`
-	Wind    SurfaceWind  `json:"wind"`
-	Clouds  []CloudLayer `json:"clouds"`
-	Vis     int          `json:"vis"`
-	Temps   TempData     `json:"temps"`
-}
+type ComponentTAF struct {
+	RawData string    `json:"raw_data"`
+	Issued  Timestamp `json:"issued"`
+	// header line:
+	//     TAF KCGI 241120Z 2412/2512 00000KT P6SM SCT250
 
-type Forecast struct {
-	RawData string `json:"raw_data"`
+	// part lines
+	//     FM241400 10005KT P6SM BKN250
+	//     FM251500 22012KT P6SM VCSH SCT020 OVC030
+	//     FM241400 17010G20KT P6SM -RA OVC035 WS020/19040KT
 }
